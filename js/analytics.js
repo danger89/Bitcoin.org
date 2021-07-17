6cca4eea5c6b725667925baa1c421ae8636427cc
Melroy van den Berg <melroy@melroy.org>
2021-07-18 00:24:36 +0200

    use my own GA

--- js/analytics.js
+++ js/analytics.js
@@ -5,7 +5,7 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-170755108-1', 'auto');
+ga('create', 'UA-202579454-1', 'auto');
 ga('send', 'pageview');
 
 /* jshint ignore:end */
